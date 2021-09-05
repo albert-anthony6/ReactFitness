0f7894b3384b24432343f9baca031cace88a0866
Albert Valdes <albert.anthony1999@gmail.com>
2021-09-05 03:18:03 -0500

    Added global error handler

--- server/app.js
+++ server/app.js
@@ -7,4 +7,26 @@ app.use(express.json());
 
 app.use('/api/v1/courses', courseRouter);
 
+app.all('*', (req, res, next) => {
+  //   res.status(404).json({
+  //     status: 'fail',
+  //     message: `Can't find ${req.originalUrl}`,
+  //   });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+  next(err);
+});
+
 module.exports = app;
