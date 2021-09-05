53c6ef32b1141f72921463b88011499d1bfc20c8
Albert Valdes <albert.anthony1999@gmail.com>
2021-09-05 03:38:58 -0500

    Created appError class for operational error handling and exported global error handler

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const globalErrorHandler = require('./controllers/errorController');
 const courseRouter = require('./routes/courseRoutes');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -8,25 +10,12 @@ app.use(express.json());
 app.use('/api/v1/courses', courseRouter);
 
 app.all('*', (req, res, next) => {
-  //   res.status(404).json({
-  //     status: 'fail',
-  //     message: `Can't find ${req.originalUrl}`,
-  //   });
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  //   const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  //   err.status = 'fail';
+  //   err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-  next(err);
-});
+app.use('*', globalErrorHandler);
 
 module.exports = app;
